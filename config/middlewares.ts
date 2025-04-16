2280b76bc5e08585b1818638fdb2edf95917c3e5
pr0secutor <ifahim3107@gmail.com>
2025-04-16 15:59:52 +0530

    rolling back the last changes

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,15 +9,4 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: 'strapi::body',
-    config: {
-      formLimit: '100mb',
-      jsonLimit: '100mb',
-    },
-  },
-  {
-    name: 'global::disable-watch',
-    config: {},
-  },
 ];
